b549f1cffbcad9101288475e5bd717485957cf73
sunjianspss <sunjianspss@gmail.com>
2025-11-22 10:28:59 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/retro-polaroid-gemini3pro/', // 这里必须必须必须改成你的 GitHub 仓库名，例如 '/my-camera-app/'
 })
 
